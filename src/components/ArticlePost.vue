51af6a756b977a0ff8cf6cff18b6f075e853697b
xuzhang0636 <363854939@qq.com>
2020-04-20 08:56:19 +0800

    article function modify

--- src/components/ArticlePost.vue
+++ src/components/ArticlePost.vue
@@ -1,6 +1,6 @@
 <template>
     <div id="editor">
-        <mavon-editor style="height: 100%;width: 100%;" v-model="content" ref=md></mavon-editor>
+        <mavon-editor style="height: 100%;width: 100%;" v-model="article.markdownContent" ref=md></mavon-editor>
         <el-button @click="saveArticle">发布</el-button>
     </div>
 </template>
@@ -8,19 +8,35 @@
 <script>
     import {mavonEditor} from 'mavon-editor'
     import 'mavon-editor/dist/css/index.css'
+    import axios from 'axios'
 
     export default {
         name: "ArticlePost",
         methods: {
             saveArticle() {
-                alert(this.$refs.md.d_render);
+                const _this = this;
+                const articleAddUrl = 'http://localhost:8081/article';
+                _this.article.htmlContent = _this.$refs.md.d_render;
+                _this.article.title = 'firstOne';
+                axios.post(articleAddUrl, _this.article).then(function () {
+                    _this.$message({
+                        type: 'success',
+                        message: '发布成功!'
+                    });
+                }).catch(function () {
+                    _this.$message({
+                        type: 'info',
+                        message: '发布失败！'
+                    })
+                });
             }
         },
         data() {
             return {
                 article: {
-                    mdContent: '',
+                    markdownContent: '',
                     htmlContent: '',
+                    title: '',
                     id: 1,
                 }
             }
